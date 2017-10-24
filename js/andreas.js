fcd39171f8e3497fb317cac43452c78054f8dcfb
Debika Sarkar <debikasarkar713@gmail.com>
2017-10-24 12:19:00 -0400

    fixed scroll

--- js/andreas.js
+++ js/andreas.js
@@ -1,7 +1,7 @@
 $('#nav-menu-container a').click(function () {
   var $href = $(this).attr('href');
   console.log($href)
-  $('body').stop().animate({
+  $('html,body').stop().animate({
     scrollTop: $($href).offset().top
   }, 1000);
   return false;
